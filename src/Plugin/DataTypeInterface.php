8f61d6fbef22998f17778f777ae204bcdb4186eb
Andy Truong <thehongtt@gmail.com>
2014-07-23 07:50:43 +0700

    Rename 'Plugin' to 'DataType' (continue)

---
+++ src/Plugin/DataTypeInterface.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace AndyTruong\TypedData\Plugin;
+
+interface DataTypeInterface
+{
+
+    /**
+     * Set definition for typed-data.
+     */
+    public function setDefinition($definition);
+
+    /**
+     * Get definition of typed-data.
+     */
+    public function getDefinition();
+
+    /**
+     * Set input for typed-data.
+     */
+    public function setInput($value);
+
+    /**
+     * Get input for typed-data.
+     */
+    public function getInput();
+
+    /**
+     * return boolean
+     */
+    public function isEmpty();
+
+    /**
+     * Validate typed-data value.
+     */
+    public function validate(&$error);
+}
